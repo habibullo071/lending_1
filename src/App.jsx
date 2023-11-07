f73d7892e6f1ffc3a522c2d67c7aed0f012b2b00
abdulloh5007 <abdulloh50007@gmail.com>
2023-11-07 15:51:05 +0500

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/Foot/Footer';
+import Header from './components/Head/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
